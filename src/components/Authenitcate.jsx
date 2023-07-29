5b9512848ac732929502c9b9493b14ad7bed504b
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-07-29 12:34:10 -0700

    tab 3 step 14 completed successfully no errors

--- src/components/Authenitcate.jsx
+++ src/components/Authenitcate.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 //Authenticate
-export default function Authenitcate() {
-  return <h2>Authenitcate!</h2>;
+export default function Authenitcate({ token }) {
+  //useStates
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+  //Async handleClick
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("handleClick works!");
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  return (
+    <div>
+      <h2>Authenitcate!</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </div>
+  );
 }

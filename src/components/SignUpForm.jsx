221600f3eec4a2a1d3f063f3268472d874cf10e8
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-07-29 10:46:37 -0700

    Completed signup components tab

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,27 +5,50 @@ export default function SignUpForm() {
   //State Variables
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, serError] = useState(null);
+  const [error, setError] = useState(null);
 
-//Handling the Submit
-async function handleSubmit(event) {
+  //Handling the Submit
+  async function handleSubmit(event) {
     event.preventDefault();
-    console.log("Hello!");
-}
 
-//Return Statement
+    //fetchAPI
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({ username, password }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  //Return Statement
   return (
     <>
       <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
-          Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <label>
-          Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <label>
-          <button >Submit</button>
+          <button>Submit</button>
         </label>
       </form>
     </>
